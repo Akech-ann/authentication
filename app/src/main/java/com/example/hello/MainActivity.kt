91186155b982ae28cf7ffcece1b1c9a0c4339afd
Akech-ann <akechannatong@gmail.com>
2023-07-06 10:07:00 +0300

    first commit

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.hello.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root) //binding activity to one layout ie linking
+
+
+        //make sure you cast before onclick listener.
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.tvlogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSignUp.setOnClickListener {
+            validateSignUp()
+        }
+    }
+
+
+    fun validateSignUp() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val pass = binding.etPassword.text.toString()
+        val confirmPassword = binding.etConfirmPassword.text.toString()
+        val email = binding.etEmail.text.toString()
+        var error = false
+
+        if (firstName.isBlank()) {
+            binding.tilFirstName.error = "First name is required"
+            error = true
+        }
+        if (lastName.isBlank()) {
+            binding.tilLastName.error = "Last name is required"
+            error = true
+        }
+        if (email.isBlank()) {
+            binding.tilEmail.error = "Email is required"
+            error = true
+        }
+        if (pass.isBlank()) {
+            binding.tillPass.error = "Password is required"
+            error = true
+        }
+        if (confirmPassword.isBlank()) {
+           binding. tilConfirmPassword.error = "password confirmation is required"
+            error = true
+        }
+        if (pass != confirmPassword) {
+            binding.tilConfirmPassword.error = "pasword and confirmation do not match"
+            error = true
+        }
+        if (!error) {
+            Toast.makeText(this, "$firstName $lastName $email", Toast.LENGTH_LONG).show()
+
+        }
+
     }
 }
